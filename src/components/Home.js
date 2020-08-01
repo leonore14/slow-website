bd83380064ad785f207e8db98c835144dfbc8d62
leonore14 <63911892+leonore14@users.noreply.github.com>
2020-08-01 15:49:29 +0200

    updated navbar

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,31 +3,32 @@ import './HomeCss.css';
 
 
 const Home = () => {
+
     return(
     <>
         <div className="home">
 
-            <div>
+            <div className="homeText1">
                 <div className="homeTitle">
                     <h1>Let's</h1>
                     <h1>Take It Slōw</h1>
                 </div>
-                <div className="homeText1">
+                <div className="homeP1">
                     <p>Graphism and Digital Studio</p>
                     <p>We take the time to listen to your wishes</p>
                     <p>to achieve great projects together.</p>
                 </div>
             </div>
 
-        </div>
+        </div >
 
         <div className="home2">
 
-            <div>
+            <div className="homeText2">
                 <div className="homeTitle2">
                     <h1>Brand Identity</h1>
                 </div>
-                <div className="homeText2">
+                <div className="homeP2">
                     <p>We propose to help you to establish your Brand Identity, creating strong logotypes and wordmarks.</p>
                     <p>We will also develop your visual identity on the different supports you may need.</p>
                 </div>
@@ -37,17 +38,19 @@ const Home = () => {
 
         <div className="home3">
 
-            <div>
+            <div className="homeText3">
                 <div className="homeTitle3">
                     <h1>Digital</h1>
                 </div>
-                <div className="homeText3">
+                <div className="homeP3">
                     <p>Being visible on Internet has become essential for any company.</p>
                     <p>We offer you to build modern and dynamic websites to facilitate and improve your development.</p>
                 </div>
             </div>
 
         </div>
+
+        <div className="home4"></div>
     
     </>
 
