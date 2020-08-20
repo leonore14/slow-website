79582dea594612bc3dced4ea1678009dfdc6ab6c
leonore14 <63911892+leonore14@users.noreply.github.com>
2020-08-20 13:59:11 +0200

    scroll effect home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,61 +1,47 @@
 import React from 'react';
 import './HomeCss.css';
+import ReactPageScroller from "../ReactpageScroller";
 
+import HomeFirstComponent from "./HomeFirstComponent";
+import HomeSecondComponent from "./HomeSecondComponent.js";
+import HomeThirdComponent from "./HomeThirdComponent";
+import HomeFourthComponent from "./HomeFourthComponent";
 
-const Home = () => {
+  
+
+class Home extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = { currentPage: null };
+      }
+    
+      handlePageChange = component => {
+        this.setState({ currentPage: component }); // set currentPage number, to reset it from the previous selected.
+      };
+    
+
+    render() {
+
 
     return(
-    <>
-        <div className="home">
+    <React.Fragment>
 
-            <div className="homeText1">
-                <div className="homeTitle">
-                    <h1>Let's</h1>
-                    <h1>Take It Slōw</h1>
-                </div>
-                <div className="homeP1">
-                    <p>Graphism and Digital Studio</p>
-                    <p>We take the time to listen to your wishes</p>
-                    <p>to achieve great projects together.</p>
-                </div>
-            </div>
-
-        </div >
-
-        <div className="home2">
-
-            <div className="homeText2">
-                <div className="homeTitle2">
-                    <h1>Brand Identity</h1>
-                </div>
-                <div className="homeP2">
-                    <p>We propose to help you to establish your Brand Identity, creating strong logotypes and wordmarks.</p>
-                    <p>We will also develop your visual identity on the different supports you may need.</p>
-                </div>
-            </div>
-
-        </div>
-
-        <div className="home3">
-
-            <div className="homeText3">
-                <div className="homeTitle3">
-                    <h1>Digital</h1>
-                </div>
-                <div className="homeP3">
-                    <p>Being visible on Internet has become essential for any company.</p>
-                    <p>We offer you to build modern and dynamic websites to facilitate and improve your development.</p>
-                </div>
-            </div>
-
-        </div>
-
-        <div className="home4"></div>
+    <ReactPageScroller
+          pageOnChange={this.handlePageChange}
+          customPageNumber={this.state.currentPage}
+    >
+         <HomeFirstComponent />
+         <HomeSecondComponent />
+         <HomeThirdComponent />
+         <HomeFourthComponent />
+       
+    </ReactPageScroller>
     
-    </>
+    </React.Fragment>
 
-    )
-    
+);
+}
 }
 
 export default Home ;
